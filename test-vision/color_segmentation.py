0a1a61c50f35f97300c9868cb239dc9b33500c5d
DanHeGa <A00838702@tec.mx>
2025-03-10 23:53:44 -0600

    distance between ball and refObj

--- test-vision/color_segmentation.py
+++ test-vision/color_segmentation.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
-import imutils #computer vision tools
+import imutils
 from imutils import contours
 from scipy.spatial import distance as dist
-from size_measure import clockwise_pts
-from size_measure import middle
+from size_measure import clockwise_pts, middle, distance
 
 cap = cv2.VideoCapture(0)
-
 cap.set(3, 640) #width
 cap.set(4, 480) #height
 cap.set(10, 20) #brightness
 
-#In HSV
-colorParams = [0,97,129,179,249,255] #most accurate HSV values for test ball (bright orange)
-referenceWidth = 10 #test width
+#in HSV 
+colorParams = [0,97,129,179,249,255]  #most accurate HSV values for test ball (bright orange) 0, 63, 255, 179, 255, 255
+refColorParams = [0, 0, 0, 0, 0, 0]  # white 
+refCenter = (320, 240) #in pixels
+referenceWidth = 2.9  # Test width
 
-def mids(img, pm, tl, tr, br, bl): #image, pixelspermetric, topleft, topright, bottomright, bottomleft
+def mids(img, ppm, tl, tr, br, bl):
     box = (tl, tr, br, bl)
     #midpoint between tl and tr coordinates
     (topX, topY) = middle(tl, tr)
@@ -38,59 +38,100 @@ def mids(img, pm, tl, tr, br, bl): #image, pixelspermetric, topleft, topright, b
     distTop2Bott= dist.euclidean((int(topX), int(topY)), (int(bottomX), int(bottomY)))
     distLeft2Right = dist.euclidean((int(leftX), int(leftY)), (int(rightX), int(rightY))) 
 
-    if pm is None:
-        pm = distLeft2Right / referenceWidth
+    if ppm is None:
+        avrgDiameterPixels = (distLeft2Right + distTop2Bott) / 2
+        ppm = avrgDiameterPixels / referenceWidth
 
     #get real size of object
-    szVert = distTop2Bott / pm
-    szHori = distLeft2Right / pm
+    szVert = distTop2Bott / ppm
+    szHori = distLeft2Right / ppm
     
     print(f"vert: {szVert}, hor: {szHori}")
-    #FALTA PONER EN TEXTO LAS DIMENSIONES
-    #cv2.putText(img, "{:.1f}cm".format(szVert), )
+    cv2.putText(img, f"{szVert:.1f}cm", (int(topX - 15), int(topY - 10)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(img, f"{szHori:.1f}cm", (int(rightX + 10), int(rightY)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
 
-
-def findContours(img, copy):
+#this function is preferable to be used when refOnj is NOT None
+def findContoursAndSize(img, copy, referenceObj=None, ppm=None):
     area, peri, radius = 0, 0, 0
-    topLeft, bottomRight = (0,0), (0,0)
+    topLeft, bottomRight = (0, 0), (0, 0)
     #contours is a list of all shapes found in a frame
-    fcontours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #fcontours = fcontours[0] if len(fcontours) == 2 else fcontours[1]
-    #fcontours = imutils.grab_contours(fcontours)###
-    if fcontours:
-        (fcontours, _) = contours.sort_contours(fcontours)####
-
-    ppm = None #pixels per metric ###
-    for cnt in fcontours:
-        area = cv2.contourArea(cnt)
-        if area > 500:
-            cv2.drawContours(copy, cnt, -1, (0,255,0), 2)
-            peri = cv2.arcLength(cnt, True) #True indicates the shape is closed
-            #Approximate the number of corners of the shape
-            box = cv2.minAreaRect(cnt)
-            box_pts = cv2.boxPoints(box)
-            box_pts = np.array(box_pts, dtype="int")
-            clockCoor = clockwise_pts(box_pts)
-            cv2.drawContours(copy, [clockCoor.astype("int")], -1, (255,255,0), 2)
-            for (x,y) in clockCoor:
-                cv2.circle(copy, (int(x), int(y)), 5, (0,0,255), -1) #circles in edges
-            mids(copy, ppm, clockCoor[0], clockCoor[1], clockCoor[2], clockCoor[3])
-            topLeft = tuple(clockCoor[0])
-            bottomRight = tuple(clockCoor[2])
-            radius = np.sqrt(area/np.pi) 
-    return radius, area, peri, topLeft, bottomRight
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
-def findColor(image, copy):
-    imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
+    if not contours:
+        return 0, 0, 0, (0, 0), (0, 0), referenceObj, ppm
+    
+    #contours = imutils.grab_contours(contours)
+    
+    # Select biggest contour
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    cnt = contours[0] #first contour (biggest)
+    
+    area = cv2.contourArea(cnt)
+    if area > 500:
+        #cv2.drawContours(copy, cnt, -1, (0, 255, 0), 2)
+        peri = cv2.arcLength(cnt, True) #True indicates the shape is closed
+        #Approximate the number of corners of the shape
+        box = cv2.minAreaRect(cnt)
+        box_pts = cv2.boxPoints(box)
+        box_pts = np.array(box_pts, dtype="int")
+        clockCoor = clockwise_pts(box_pts)
+        cX = np.average(clockCoor[:, 0])
+        cY = np.average(clockCoor[:, 1]) #np.average(clockCoor[:, 1])
+        #if distance has refObj as NOT none, it will calculate the distance between the refOnj and the Obj (ball in this case)
+        referenceObj = distance(copy, referenceObj, clockCoor, (cX, cY))
+        cv2.drawContours(copy, [clockCoor.astype("int")], -1, (255, 255, 0), 2)
+        for (x, y) in clockCoor:
+            cv2.circle(copy, (int(x), int(y)), 5, (0, 0, 255), -1) # circles in edges
+        mids(copy, ppm, clockCoor[0], clockCoor[1], clockCoor[2], clockCoor[3]) #mid lines in shape, includes size
+        topLeft = tuple(clockCoor[0])
+        bottomRight = tuple(clockCoor[2])
+        radius = np.sqrt(area / np.pi)
+    
+    return radius, area, peri, topLeft, bottomRight, referenceObj, ppm
+
+def findColor(image, copy, referenceObj=None, ppm=None):
+    imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Detect reference object, used section in case of going with objects rather than center of image
+    # Mask for refreence object
+    lower_ref = np.array(refColorParams[0:3])
+    upper_ref = np.array(refColorParams[3:6])
+    mask_ref = cv2.inRange(imgHSV, lower_ref, upper_ref) #create a mask with an accepted range of HSV values
+      
+    contours_ref, _ = cv2.findContours(mask_ref, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # if there is no refObject yet
+    if contours_ref and referenceObj is None: 
+        contours_ref = sorted(contours_ref, key=cv2.contourArea, reverse=True)
+        for cnt in contours_ref:
+            area = cv2.contourArea(cnt)
+            if area > 500:
+                box = cv2.minAreaRect(cnt)
+                box_pts = cv2.boxPoints(box)
+                box_pts = np.array(box_pts, dtype="int")
+                clockCoor_ref = clockwise_pts(box_pts)
+                cX_ref = np.average(clockCoor_ref[:, 0])
+                cY_ref = np.average(clockCoor_ref[:, 1])
+                #defines the refObject
+                referenceObj = distance(copy, None, clockCoor_ref, (cX_ref, cY_ref))
+                cv2.drawContours(copy, [clockCoor_ref.astype("int")], -1, (255, 0, 255), 2)
+                cv2.circle(copy, (int(cX_ref), int(cY_ref)), 5, (0, 255, 255), -1)
+                break
+    # Ball mask, already have refObj
     lower = np.array(colorParams[0:3])
     upper = np.array(colorParams[3:6])
-    mask = cv2.inRange(imgHSV, lower, upper) #create a mask with an accepted range of HSV values
-    r, a, p, top_left, bottom_right= findContours(mask, copy) #returns bounding box, area, peri, and radius
-    #cv2.rectangle(copy, top_left, bottom_right, (0, 255, 0), 5) 
-    cv2.putText(copy, "Ball",(bottom_right[0], bottom_right[1]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,0), 2)
-    if a > 100: # just to see test objects
-        print(f"Area: {a} Perimeter: {p} Radius: {r}")
+    
+    #img preprocessing
+    mask = cv2.inRange(imgHSV, lower, upper)
+    mask = cv2.erode(mask, None, iterations=1)
+    mask = cv2.dilate(mask, None, iterations=1)
+
+    r, a, p, top_left, bottom_right, referenceObj, ppm = findContoursAndSize(mask, copy, referenceObj, ppm)
+
     cv2.imshow("mask", mask)
+    #returns refObj(if object used) and ppm for those global variables to be in constant and correct changing state.
+    return referenceObj, ppm
+
+referenceObj = None
+ppm = None
 
 while True:
     success, img = cap.read()
@@ -99,7 +140,7 @@ while True:
         #image preprocessing
         img_blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-        findColor(img_blur, img_copy)
+        referenceObj, ppm = findColor(img_blur, img_copy, referenceObj, ppm)
 
         cv2.imshow("Test", img_copy)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- test-vision/size_measure.py
+++ test-vision/size_measure.py
@@ -6,7 +6,10 @@ import argparse #for console arguments **Not necessary**
 import imutils
 import cv2
 
-#returns the middle between two points
+REFOBJ_WIDTH = 1 #cm, CHECK IF U ARE MANAGING METRICS CORRECTLY, OR DO I HAVE TO PASS FROM FT TO IN OR SMTNG
+color = (255,255,255) #white
+
+#returns the middle of two points
 def middle(point1, point2):
     return((point1[0] + point2[0]) * 0.5, (point1[1] + point2[1]) * 0.5)
 
@@ -17,7 +20,7 @@ def clockwise_pts(pts):
     xSort = pts[np.argsort(pts[:,0]), :]
     leftMost = xSort[:2, :] #select the first two rows and all the colums of it.
     rightMost = xSort[2:, :]
-
+    
     leftmost = leftMost[np.argsort(leftMost[:, 1]), :] #sort them by their Y coordinate
     tl, bl = leftmost
     #print(f"sorted tl: {tl} bl: {bl}")
@@ -27,6 +30,7 @@ def clockwise_pts(pts):
 
     return np.array([tl, tr, br, bl])
 
+#not used yet
 def measure_size(img, copy): 
     '''gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur_gray = cv2.GaussianBlur(gray_image, (7, 7), 0)
@@ -39,7 +43,6 @@ def measure_size(img, copy):
 
     #contouring
     fcontours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #fcontours = imutils.grab_contours(fcontours) #manages contours in an easier way (doesn't matter opencv version)
 
     #useful for left reference objects, sorts them from left to right
     (fcontours, _) = contours.sort_contours(fcontours)
@@ -58,10 +61,40 @@ def measure_size(img, copy):
         #for (x, y) in cnt:
          #   cv2.circle(copy, (int(x), int(y)), 2, (255, 0, 0), 2)
 
+#Returns a refObject, calculates distance between two objects if refObj already exist
+def distance(imgCopy, refObj,objCoor,  objCenter): #objCoorX and Y are teh center of other object that is NOT the reference one, for example, the ball
+    #these three lines are not used when refObj is None
+    centerX = np.average(objCoor[:,0])
+    centerY = np.average(objCoor[:,1])
+    ballCenter = objCenter
 
-#test loop
+    if refObj is None:
+        #if NO refObj, creates one with the actual taken coordinates (while in a for loop)
+        tl, tr, br, bl = objCoor
+        centerLeftX, centerLeftY = middle(tl, bl)
+        centerRightX, centerRightY = middle(tr, br)
 
+        distHorizon = dist.euclidean((centerLeftX, centerLeftY), (centerRightX, centerRightY))
+        #declare reference object with it's box, it's center and ppm.
+        refObj = (objCoor, (centerX, centerY), distHorizon / REFOBJ_WIDTH)
+    else:
+        refBox, refCenter, ppm = refObj
+    
+        cv2.drawContours(imgCopy, [refBox.astype("int")], -1, (255,0,255), 2)
+        cv2.drawContours(imgCopy, [objCoor.astype("int")], -1, (0,0,0), 2)
 
+        #circles to create connections
+        cv2.circle(imgCopy,(int(refCenter[0]),int(refCenter[1])), 5, color, -1)
+        cv2.circle(imgCopy, (int(ballCenter[0]), int(ballCenter[1])), 5, color, -1)
+        cv2.line(imgCopy, (int(refCenter[0]),int(refCenter[1])), (int(ballCenter[0]), int(ballCenter[1])), color, 2)
+
+        #get the distance (in  pixels) from one pt to another
+        distAB = dist.euclidean(refCenter, objCenter) #falta poner en ppm, antes lo dividias entre ppm del refObj
+        midX, midY = middle(refCenter, objCenter)
+        cv2.putText(imgCopy, "{:.1f}cm(IN)".format(distAB), (int(midX), int(midY) - 10), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,255), 2)
+    
+    return refObj
+    
         
 
 
