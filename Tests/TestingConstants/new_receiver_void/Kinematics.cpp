81b9709bd2681fb9227e09fed62eaced98239ec4
EfrainH2O <112781053+EfrainH2O@users.noreply.github.com>
2025-03-22 11:42:25 -0600

    TestingConstants

---
+++ Tests/TestingConstants/new_receiver_void/Kinematics.cpp
@@ -0,0 +1,77 @@
+#include "Arduino.h"
+#include "Kinematics.h"
+
+Kinematics::Kinematics(float motor_max_rpm, float wheel_diameter, float lr_wheels_dist, int pwm_bits, int vel, int theta){
+  circumference_ = PI * wheel_diameter;
+  max_rpm_ = motor_max_rpm;
+  lr_wheels_dist_ = lr_wheels_dist;
+  pwm_res_  = pow ( 2, pwm_bits) - 1;
+  max_vel = (float)motor_max_rpm * circumference_;
+  radius = wheel_diameter/2;
+  ConstThetaDiff = theta;
+  ConstVelDiff = vel;
+}
+  
+  
+
+output Kinematics::getRPM(velocities ObVel) // the objective velocityes x,y,theta
+{
+//For now it is that x is the force and z the angule. However this would change depending on the development we could do later (odometry)
+  
+  float ObVelMagnitude = ObVel.Magnitude() * ConstVelDiff; //ToGet the magnitude of the force
+  ObVel.setAngule();
+  ObVel._z *= ConstThetaDiff;
+  
+  //Funciton to determine the vel of each wheel with the values of vel and theta dif
+  float leftVel = ObVelMagnitude/ radius - lr_wheels_dist_ / (2*radius) * ObVel._z; 
+  float rightVel = ObVelMagnitude/ radius + lr_wheels_dist_ / (2*radius) * ObVel._z;
+
+  //Change the data to rpm insted of m/s;
+  output rpm;
+  rpm.motor1 = leftVel/circumference_  * 60;
+  rpm.motor2  = rightVel / circumference_ * 60;
+  rpm.Scale(max_rpm_);
+  return rpm;
+}
+
+output Kinematics::getPWM(velocities ObVel)
+{
+  output rpm;
+  output pwm;
+
+  rpm = getRPM(ObVel);
+
+  //convert from RPM to PWM
+  pwm = rpmToPWM(rpm);
+
+  return pwm;
+}
+//Function to determine the velocities of the robot in the x and y plane with the real wheel velocities
+velocities Kinematics::getVelocities(output actualRPM, float theta)
+{
+  velocities vel;
+  //Change from rpm to m/s
+  actualRPM.motor1 = (actualRPM.motor1 *circumference_ / 60);
+  actualRPM.motor2 = (actualRPM.motor2 *circumference_ / 60);
+
+  //With the formulas we determine the velocities and send them;
+
+  vel._x = (radius * actualRPM.motor1)/2 *cos(theta) + (radius * actualRPM.motor2) /2 *cos(theta);
+  vel._y = (radius * actualRPM.motor1)/2 *sin(theta) + (radius * actualRPM.motor2) /2 *sin(theta);
+  vel._z = -radius / lr_wheels_dist_ * actualRPM.motor1 +  radius / lr_wheels_dist_ * actualRPM.motor2; 
+
+
+  return vel;
+
+
+}
+
+
+
+output Kinematics::rpmToPWM(output rpm)
+{
+  //remap scale of target RPM vs MAX_RPM to PWM
+  rpm.motor1 = ((rpm.motor1 /  max_rpm_) * pwm_res_);
+  rpm.motor2 = ((rpm.motor2 /  max_rpm_) * pwm_res_);
+  return rpm;
+}
---
+++ Tests/TestingConstants/new_receiver_void/Kinematics.h
@@ -0,0 +1,91 @@
+
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
+
+#include "Arduino.h"
+
+
+class output
+{
+  public:
+  float motor1; //left
+  float motor2; //right
+  //Scale function to Chanage the maximum value and continue to have the proportion of the force
+  void Scale(float MaxRPM){
+    if(abs(motor1) > MaxRPM || abs(motor2) > MaxRPM){
+      if(abs(motor1) > abs(motor2)){
+        float scale = MaxRPM/abs(motor1);
+        motor1 = MaxRPM * (signbit(motor1) ? -1 : 1);
+        motor2 = motor2 * scale;
+      }else{
+        float scale = MaxRPM/abs(motor2);
+        motor2 = MaxRPM * (signbit(motor2) ? -1 : 1);
+        motor1 = motor1 * scale;
+      }
+    }
+  }
+};
+class velocities
+{
+  public:
+  float _x;
+  float _y;
+  float _z;
+  float Magnitude()
+  {
+    return sqrt(pow (_x, 2) + pow(_y, 2));
+  }
+  //Get the angle of a vector if it is in a x y form
+  void setAngule(){
+    if(_x  == 0) {
+      _z = _y > 0 ? PI/2 : 3*PI/2;
+    }else if(_y == 0){
+      _z = _x > 0 ? 0 : PI;
+    }else{
+        _z = atan(_y/_x);                
+        _z = _x > 0? (_z < 0? 2*PI + _z : _z) : PI + _z;   
+    }
+
+  }
+  //Get the least difference between angles
+  float getThetaDif(float desire, float actual){
+      float dif = desire - actual;
+      while(abs(dif) > 2*PI){
+        dif /= 2*PI;
+      }
+      if(abs(dif) < PI){
+        return dif;
+      }else{
+        return (signbit(dif) ? 1 : -1) * 2*PI + dif;
+      }
+  }
+  
+
+};
+
+
+class Kinematics
+{
+  public:
+    
+    Kinematics(float motor_max_rpm, float wheel_diameter, float lr_wheels_dist, int pwm_bits, int ConstVelDiff, int  ConstThetaDiff);
+    velocities getVelocities(output actualVel, float theta);
+    output getRPM(velocities);
+    output getPWM(velocities);
+    output rpmToPWM(output);
+    float max_vel;
+
+  private:
+    float radius;
+    float circumference_;
+    int max_rpm_;
+    float lr_wheels_dist_;
+    float pwm_res_;
+    int ConstThetaDiff;
+    int ConstVelDiff;
+};
+
+
+
+
+#endif
