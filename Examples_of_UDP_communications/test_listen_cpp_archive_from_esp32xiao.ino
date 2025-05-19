611c3dbb05648b5918ae64855c7dfdac16e63cbb
bandarcrono <A00841456@tec.mx>
2025-05-19 17:15:01 -0600

    Refactor ESP32 UDP example: update Wi-Fi credentials, clean up commented code, and replace outdated motor drive function with Drive2.

--- Examples_of_UDP_communications/test_listen_cpp_archive_from_esp32xiao.ino
+++ Examples_of_UDP_communications/test_listen_cpp_archive_from_esp32xiao.ino
@@ -4,32 +4,22 @@ WiFiUDP udp;
 
 
 //Wi-Fi credentials
-const char* ssid = "CRONO 7087";
-const char* password = "&839a33N";
-//IPAddress otherDeviceIP(192,168,1,6); // Replace with computer id
-
+const char* ssid = "EXAMPLE_SSID"; // Replace with your Wi-Fi SSID'";
+const char* password = "EXAMPLE_PASSWORD"; // Replace with your Wi-Fi password
 
 unsigned int localUdpPort = 1001;  // Port to listen on
 char incomingPacket[255];          // Buffer for incoming packets
 
-
-/*
-float x_coord ;
-float y_coord ;
-*/
-
 // TB6612FNG Motor Driver pins
 #define MotorA1 2 
-#define MotorA2 21 // new prototype 
+#define MotorA2 21
 #define MotorA_PWM 0
-
 
 #define MotorB1 17
 #define MotorB2 19
 #define MotorB_PWM 1
 
 
-/*
 void Drive2(int b, int a){
 
   if (a > 0){
@@ -54,8 +44,9 @@ void Drive2(int b, int a){
     ledcWrite(MotorB2,0);
   }
 }
-*/
 
+//Outdated example of a motor drive function 
+/*
 void Drive(int MotorL, int MotorR) {
   // Control left motor (Motor A)
   if (MotorL > 0) {
@@ -85,7 +76,7 @@ void Drive(int MotorL, int MotorR) {
   ledcWrite(MotorB_PWM, abs(MotorR));
   //Serial.print(MotorL);
 }
-
+*/
 
 void setup() {
   pinMode(MotorA1, OUTPUT);
@@ -95,10 +86,7 @@ void setup() {
   ledcAttach(MotorA_PWM, 10000, 8);
   ledcAttach(MotorB_PWM, 10000, 8);
 
-
-
   // Configure motor pins
-  /*
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -108,11 +96,9 @@ void setup() {
   Serial.println("\nConnected to Wi-Fi");
   Serial.printf("Device IP: %s, UDP port: %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
   udp.begin(localUdpPort);  // Start UDP
-  */
 }
 
 void loop() {
-  /*
   int packetSize = udp.parsePacket();
   if (packetSize) {
     // Read the packet
@@ -121,18 +107,17 @@ void loop() {
     // Check if we received exactly 8 bytes (two floats, 4 bytes each)
     if (len == 8) {
       // Extract the two float values from the packet
-      memcpy(&x_coord, incomingPacket, 4);
-      memcpy(&y_coord, incomingPacket + 4, 4);
+      memcpy(&RPM, incomingPacket, 4);
+      memcpy(&RPM2, incomingPacket + 4, 4);
       
       // Print the received coordinates
       Serial.printf("Received coordinates: x=%.2f, y=%.2f from %s\n", 
-                    x_coord, y_coord, 
+                    RPM, RPM2, 
                     udp.remoteIP().toString().c_str());
     }
     else {
       Serial.println("Received packet with unexpected size");
     }
   }
-  */
-  Drive(255,255);  
+  Drive2(RPM,RPM2);  
 }
